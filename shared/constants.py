e14bcebf7208f9d962199a0925b3ac08ad96ad85
Mel Edward Dasco <95231423+meldasco@users.noreply.github.com>
2023-10-26 01:02:24 +0800

    Delete shared directory

--- shared/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-
-cw_dir = os.getcwd()
-output_folder = cw_dir + '\\' + 'output' + '\\'
-diff_report = output_folder + 'diff_report.xlsx'
-date_range_report = output_folder + 'date_range_report.xlsx'
-date_today_report = output_folder + 'today_report.xlsx'
